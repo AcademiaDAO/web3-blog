6f34673f5850c357a98cd1ece75184d290d49090
shungo11260 <shungotaniguchi1120@gmail.com>
2022-03-06 18:27:13 -0800

    set mubai network in hardhat conf

--- hardhat.config.js
+++ hardhat.config.js
@@ -22,10 +22,10 @@ module.exports = {
     hardhat: {
       chainId: 1337,
     },
-    // mumbai: {
-    //   url: "https://rpc-mumbai.matic.today",
-    //   accounts: [process.env.pk]
-    // },
+    mumbai: {
+      url: "https://polygon-mumbai.g.alchemy.com/v2/kiowIrctiSmIvZ0qvQTpjd45S2opbqu8",
+      accounts: [process.env.pk]
+    },
     // polygon: {
     //   url: "https://polygon-rpc.com/",
     //   accounts: [process.env.pk]
